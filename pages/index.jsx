889ff5e73b1de5f1f229bbdca78e1bea5e5f6f4e
Gennadiy Pospelov <gennadiypospelov@Gennadiys-iMac.lan>
2022-04-06 15:35:26 +0300

    layout scratch

--- pages/index.jsx
+++ pages/index.jsx
@@ -6,8 +6,8 @@ export default function Home() {
   return (
     <Layout>
       <Container xs={{ display: 'none' }}>
-        <Box sx={{ paddingTop: { xs: 3, xl: 6 }, textAlign: 'center' }}>
-          <Typography variant="h3">Украшения ручной работы</Typography>
+        <Box sx={{ paddingTop: { xs: 3, xl: 9 }, textAlign: 'center' }}>
+          <Typography variant="h4">Украшения ручной работы</Typography>
         </Box>
         <Box sx={{ paddingTop: { xs: 3, xl: 6 }, textAlign: 'center' }}>
           <Typography variant="h6">
