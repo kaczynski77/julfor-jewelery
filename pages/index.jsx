3296f78e0ee06970e547739160ce07d7d5662a0c
Gennadiy Pospelov <gennadiypospelov@Gennadiys-iMac.lan>
2022-04-06 16:28:01 +0300

    layout scratch fix

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,21 +1,24 @@
 import { Container, Typography, Box } from '@mui/material';
 import Button from '@mui/material/Button';
 import Layout from '../components/layout';
+import { grey } from '@mui/material/colors';
 
 export default function Home() {
   return (
     <Layout>
       <Container xs={{ display: 'none' }}>
-        <Box sx={{ paddingTop: { xs: 3, xl: 9 }, textAlign: 'center' }}>
-          <Typography variant="h4">Украшения ручной работы</Typography>
+        <Box sx={{ paddingTop: { xs: 2, xl: 9 }, textAlign: 'center' }}>
+          <Typography variant="h5" sx={{ textTransform: 'uppercase' }}>
+            украшения ручной работы
+          </Typography>
         </Box>
         <Box
           sx={{
-            paddingTop: { xs: 3, xl: 6 },
-            textAlign: { xs: 'left', sm: 'center' },
+            paddingTop: { xs: 2, xl: 3 },
+            textAlign: { xs: 'center', sm: 'center' },
           }}
         >
-          <Typography variant="h6">
+          <Typography sx={{ color: grey[400] }} variant="overline">
             Изящество, легкость, неповторимый стиль наших украшений. Самые
             современные тренды и высокое качество. Все это помогает вам создать
             женственный образ.
