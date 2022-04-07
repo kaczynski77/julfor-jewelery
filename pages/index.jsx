3354854c17afc904f42c497e057bf06194158043
Gennadiy Pospelov <gennadiypospelov@Gennadiys-iMac.lan>
2022-04-07 12:04:02 +0300

    layout day2 main page styles

--- pages/index.jsx
+++ pages/index.jsx
@@ -7,13 +7,21 @@ import MainCategories from '../components/mainCategories';
 import MainHits from '../components/mainHits';
 import MainServices from '../components/mainServices';
 import MainBanner from '../components/mainBanner';
+import { borderLeft } from '@mui/system';
 
 export default function Home() {
   return (
     <Layout>
       <Container xs={{ display: 'none' }}>
         <Box sx={{ paddingTop: { xs: 0, xl: 8 }, textAlign: 'center' }}>
-          <Typography variant="h3" sx={{ textTransform: 'uppercase' }}>
+          <Typography
+            variant="h3"
+            sx={{
+              textTransform: 'uppercase',
+              fontSize: 28,
+              fontWeight: 'bold',
+            }}
+          >
             украшения ручной работы
           </Typography>
         </Box>
@@ -21,10 +29,12 @@ export default function Home() {
           sx={{
             paddingTop: { xs: 2, xl: 1 },
             textAlign: { xs: 'center', sm: 'center' },
+            maxWidth: { xs: 400, sm: 680 },
+            margin: '0 auto',
           }}
         >
           <Typography
-            sx={{ color: grey[900], fontSize: 16 }}
+            sx={{ color: grey[900], fontSize: 14, lineHeight: 1 }}
             variant="overline"
           >
             Наши украшения раскрывают женственность и привлекательность Для
